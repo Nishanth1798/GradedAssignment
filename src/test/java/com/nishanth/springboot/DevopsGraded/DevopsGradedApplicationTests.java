39a94e296fb8b31922a34928c528cc3835c4a307
student <gone.nishanth@hcl.com>
2021-04-21 13:14:45 +0000

    first commit

---
+++ src/test/java/com/nishanth/springboot/DevopsGraded/DevopsGradedApplicationTests.java
@@ -0,0 +1,13 @@
+package com.nishanth.springboot.DevopsGraded;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DevopsGradedApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
